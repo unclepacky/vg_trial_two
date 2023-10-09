cbcd4808d164ae9a0839665c93048363f792561f
UnclePacky <claudeaoun@gmail.com>
2023-10-09 21:55:57 +0300

    seeding a customer

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,35 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { CustomerType, Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const customerData: Prisma.CustomerCreateInput[] = [
+  {
+    firstName: "claude",
+    LastName: "Aoun",
+    displayName: "UnclePacky",
+    email: "claudeaoun@gmail.com",
+  },
+];
+
 async function main() {
-  const customer = await prisma.customer.findMany();
-  console.log(customer);
+  await prisma.customer.deleteMany();
+  //   const customer = await prisma.customer.create({
+  //     data: {
+  //       firstName: "claude",
+  //       LastName: "Aoun",
+  //       displayName: "UnclePacky",
+  //       email: "claudeaoun@gmail.com",
+  //     },
+  //   });
+  //   const customer = await prisma.customer.findMany();
+  //   console.log(customer);
+
+  for (const u of customerData) {
+    const customer = await prisma.customer.create({
+      data: u,
+    });
+    console.log(`Customer with id: ${customer.id} was created.`);
+  }
 }
 
 main()
